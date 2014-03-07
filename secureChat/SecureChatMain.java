36a402296d44e1fb7cae547250f7c1d2472e0961
richard csula <richard.csdept@gmail.com>
2014-03-06 17:00:52 -0800

    first set of source files

---
+++ secureChat/SecureChatMain.java
@@ -0,0 +1,12 @@
+package secureChat;
+import java.util.*;
+
+
+public class SecureChatMain {
+
+	public static void main(String[] args) {
+		System.out.println("top secret");
+
+	}
+
+}
---
+++ secureChat/SecureChatServer.java
@@ -0,0 +1,5 @@
+package secureChat;
+
+public class SecureChatServer {
+
+}
